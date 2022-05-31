920461bd27390d2dd473c3487778eaed8cc0a8b4
LimaGustav <lima.gustavo100@hotmail.com>
2022-05-31 08:59:05 -0300

    Administrador crud

--- src/pages/Administrador.js
+++ src/pages/Administrador.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import { Link } from 'react-router-dom';
 import editIcon from '../assets/edit.svg'
@@ -7,8 +7,22 @@ import './css/administrador.css'
 
 import admEdit from '../assets/adm_edit.svg'
 import admDelete from '../assets/adm_delete.svg'
+import api from '../services/api';
 
 const Administrador = () => {
+    const [colaboradores, setColaboradores] = useState([]);
+
+    function BuscarColaboradores() {
+        api.get("/Usuarios").then(response => {
+            if (response.status === 200) {
+                setColaboradores(response.data)
+            }
+            console.log(response.data)
+        }).catch(erro => console.log(erro))
+    }
+
+    useEffect(BuscarColaboradores, [])
+
     return (
         <div>
             <Header></Header>
@@ -26,18 +40,22 @@ const Administrador = () => {
                 <hr className='cl_hrTitle' />
             </div>
 
-            <div className='cl_container_client grid'>
-                <div className='cl_table_head cl_table_client adm_table_user'>
-                    <span className='cl_cliente_text'>Gustavo</span>
-                    <p className='cl_descricao_text adm_tipo_text'>Administrador</p>
-                    <span className='cl_status_text'>lima.gustavo100@usp.br</span>
-                    <div className="adm_icon_container">
-                        <img className='pointer' src={admEdit} alt="icone de editar usuário" />
-                        <img className="pointer" src={admDelete} alt="icone de deletars usuário" />
+            {colaboradores.map(colaborador => {
+                return (
+                    <div className='cl_container_client grid'>
+                        <div className='cl_table_head cl_table_client adm_table_user'>
+                            <span className='cl_cliente_text'>{colaborador.nome}</span>
+                            <p className='cl_descricao_text adm_tipo_text'>cola</p>
+                            <span className='cl_status_text'>{colaborador.email}</span>
+                            <div className="adm_icon_container">
+                                <img className='pointer' src={admEdit} alt="icone de editar usuário" />
+                                <img className="pointer" src={admDelete} alt="icone de deletars usuário" />
+                            </div>
+                        </div>
+                        <hr className='cl_hrCliente' />
                     </div>
-                </div>
-                <hr className='cl_hrCliente' />
-            </div>
+                )
+            })}
         </div>
     );
 }
